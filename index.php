377e0500d208a9c47f077ae04657a5048d89e431
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2024-10-24 00:41:48 +0530

    array

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<form action="index.php" method="get">
-
-    <label>Username:</label><br>
-    <input type="text" name="username"><br>
-    <label>Password:</label><br>
-    <input type="password" name="password"><br>
-    <input type="submit" value="Log in"><br>
-</form>
-
 <?php
 
-echo "{$_GET["username"]} <br>";
-echo "{$_GET["password"]} <br>";
+$name = array("1","2","3","4");
 
+print_r($name);
 ?>
-
-</body>
-</html>
